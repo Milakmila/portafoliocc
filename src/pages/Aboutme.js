f3b4c8ba2da2f3a1731455c730289dcb002345ed
milakmila <camilaleja.cipa@gmail.com>
2022-05-21 18:47:38 -0500

    Add of aboutme pages

---
+++ src/pages/Aboutme.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header/Header";
+import Footer from "../components/footer/Footer";
+import Aboutme from "../components/Aboutme/Aboutme";
+function About(){
+    return(
+        <div>
+             <Header />
+            <Aboutme />
+            <Footer />
+        
+        </div>
+    )
+    }
+    
+    export default About;
