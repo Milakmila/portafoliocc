1f259d84a1dcbd18098112b7e772fb37f9f025af
milakmila <camilaleja.cipa@gmail.com>
2022-05-23 16:07:02 -0500

    Update portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,7 +11,7 @@ function Project(){
             <div className="card-content">
                 <h3 className="card-title">Figma prototipo</h3>
                 <p className="card-description">Desarrollo de un prototipo en figma de una agencia de hoteles y viajes</p>
-                <a href="https://www.figma.com/proto/qY9egtj1F7GnwJUCiZLtXm/Protipo_ejemplo?node-id=0%3A1&scaling=scale-down&page-id=0%3A1">IR</a>
+                <a className="ir" href="https://www.figma.com/proto/qY9egtj1F7GnwJUCiZLtXm/Protipo_ejemplo?node-id=0%3A1&scaling=scale-down&page-id=0%3A1">IR</a>
 
             </div>
         </div>
@@ -20,7 +20,7 @@ function Project(){
             <div className="card-content">
                 <h3 className="card-title">DuaLipa</h3>
                 <p className="card-description">Fandom de DuaLipa su historia, una linea de tempo y ganar unas bolestas para su concierto</p>
-                <a href="https://milakmila.github.io/DuaLipa/">IR</a>
+                <a className="ir"  href="https://milakmila.github.io/DuaLipa/">IR</a>
 
             </div>
         </div>
@@ -29,7 +29,7 @@ function Project(){
             <div className="card-content">
                 <h3 className="card-title">Forja</h3>
                 <p className="card-description">Proyecto Forja para el registro de vacunas de los enfermeros </p>
-                <a href="https://milakmila.github.io/Forja-empresas-m3/">IR</a>
+                <a className="ir"  href="https://milakmila.github.io/Forja-empresas-m3/">IR</a>
 
             </div>
         </div>
@@ -37,15 +37,15 @@ function Project(){
             <div className="card-content">
                 <h3 className="card-title">Bootstrap Animalandia</h3>
                 <p className="card-description">Proyecto de veterinaria llamada Animalandia en Bootstrap</p>
-                <a href="https://milakmila.github.io/Bootstrap-Animalandia/">IR</a>
+                <a className="ir"  href="https://milakmila.github.io/Bootstrap-Animalandia/">IR</a>
 
             </div>
         </div>
         <div className="card-five">
             <div className="card-content">
-                <h3 className="card-title">Design thinking para Forja</h3>
+                <h3 className="card-title">Design thinking</h3>
                 <p className="card-description">Proceso de desarollo de ideacion y personalidad de marca para el proceso de diseño web para Forja</p>
-                <a href="https://www.canva.com/design/DAE4oLSLy9I/DDvx8nS7-311YleVmz-c3Q/view?utm_content=DAE4oLSLy9I&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">IR</a>
+                <a className="ir"  href="https://www.canva.com/design/DAE4oLSLy9I/DDvx8nS7-311YleVmz-c3Q/view?utm_content=DAE4oLSLy9I&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">IR</a>
 
             </div>
         </div>
@@ -53,7 +53,7 @@ function Project(){
             <div className="card-content">
                 <h3 className="card-title">Shoes Room</h3>
                 <p className="card-description">Prototipo de una remontadora de zapatos en FIgma</p>
-                <a href="https://www.figma.com/proto/Fv9Txo25JQUPNo68Mb7zUX/High-fidelity-Wireframe_-Shoes-room?node-id=3%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=3%3A2&show-proto-sidebar=1">IR</a>
+                <a className="ir"  href="https://www.figma.com/proto/Fv9Txo25JQUPNo68Mb7zUX/High-fidelity-Wireframe_-Shoes-room?node-id=3%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=3%3A2&show-proto-sidebar=1">IR</a>
 
             </div>
         </div>
